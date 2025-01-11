fa260a4e6d06a243b16a7af3bfefbfa132512657
Vedant Zope <vedantzope9@gmail.com>
2025-01-11 16:06:49 +0530

    Chat Model updated

--- ChatApp/src/main/java/org/vz/chatapp/model/ChatMessage.java
+++ ChatApp/src/main/java/org/vz/chatapp/model/ChatMessage.java
@@ -3,12 +3,41 @@ package org.vz.chatapp.model;
 import lombok.*;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class ChatMessage {
-    //private Long id;
+    private Long id;
     private String sender;
     private String content;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(Long id, String sender, String content) {
+        this.id = id;
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
